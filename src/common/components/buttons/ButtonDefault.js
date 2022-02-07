fa40326c9c217bcabcaeef792922a278562b9cb6
Jinho <breezjin@gmail.com>
2022-02-08 00:50:41 +0900

    [54] 헤더 작업을 위한  버튼 컴포넌트 업데이트

--- src/common/components/buttons/ButtonDefault.js
+++ src/common/components/buttons/ButtonDefault.js
@@ -7,17 +7,22 @@ import right from "../../../assets/icon-right.png";
 
 const StyledButtonDefault = styled.button`
   width: fit-content;
+  height: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: medium;
   color: #bc955c;
   background-color: black;
-  padding: 10px 25px;
+  padding: 10px;
   border-radius: 3px;
+  .icon {
+    margin-top: 2px;
+    margin-right: 0.5rem;
+  }
 `;
 
-function ButtonDefault({ type, onClick, moveTo, disabled, children }) {
+function ButtonDefault({ type, onClick, moveTo, disabled, icon, children }) {
   return (
     <StyledButtonDefault
       type={type}
@@ -26,7 +31,12 @@ function ButtonDefault({ type, onClick, moveTo, disabled, children }) {
       disabled={disabled}
     >
       {moveTo === "left" && <img src={left} alt="뒤로가기" />}
-      {children}
+      {icon && (
+        <div className="icon">
+          <img src={icon} alt="아이콘" />
+        </div>
+      )}
+      {children && children}
       {moveTo === "right" && <img src={right} alt="앞으로가기" />}
     </StyledButtonDefault>
   );
@@ -37,11 +47,14 @@ ButtonDefault.propTypes = {
   onClick: PropTypes.func.isRequired,
   moveTo: PropTypes.string,
   disabled: PropTypes.bool.isRequired,
-  children: PropTypes.string.isRequired,
+  icon: PropTypes.element,
+  children: PropTypes.string,
 };
 
 ButtonDefault.defaultProps = {
   moveTo: "none",
+  icon: null,
+  children: null,
 };
 
 export default ButtonDefault;
