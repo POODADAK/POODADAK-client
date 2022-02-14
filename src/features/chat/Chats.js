fa0e9fc772c08f15c24a675de6fa5d8ea3fe51f0
Jinho <breezjin@gmail.com>
2022-02-15 05:39:08 +0900

    [69] 채팅 리스트 프론트 구현

--- src/features/chat/Chats.js
+++ src/features/chat/Chats.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 import HeaderSub from "../../common/components/headers/HeaderSub";
@@ -14,22 +16,50 @@ const StyledChats = styled.div`
   background-color: black;
 `;
 
+const BASE_URL = process.env.REACT_APP_AXIOS_BASE_URL;
+
 function Chats() {
   const navigate = useNavigate();
-  const location = useLocation();
-  const toilet = location.state;
+  const userId = useSelector((state) => state.login.userId);
+  const [chatrooms, setChatrooms] = useState([]);
+
+  async function getToilet(toiletId) {
+    const toilet = await axios.get(`${BASE_URL}/toilets/toilet/${toiletId}`);
+
+    return toilet;
+  }
+
+  function findLastChat(chatList) {
+    const lastChat = chatList[chatList.length - 1];
+
+    return lastChat;
+  }
+
+  useEffect(() => {
+    async function findMyChatRoooms() {
+      const myChatrooms = await axios.get(
+        `${BASE_URL}/chatroom/live-chatroom/${userId}`
+      );
+
+      setChatrooms(myChatrooms);
+    }
+
+    if (userId) findMyChatRoooms();
+  }, [userId]);
 
   return (
     <StyledChats>
       <HeaderSub onClick={() => navigate("/chats")} />
-      <Title
-        title={toilet ? `${toilet.name} 채팅 목록` : "{toilet.name} 채팅 목록"}
-      />
-      {/* <List2Lines
-        label={user ? user.name : "{user.name}"}
-        secondary={chat ? chat.latestMsg : "{chat.latestMsg}"}
-      /> */}
-      <List2Lines label="{user.name}" secondary="{chat.latestMsg}" />
+      {chatrooms &&
+        chatrooms.map((chatroom) => (
+          <>
+            <Title title={getToilet(chatroom.toilet).toiletName} />
+            <List2Lines
+              label={findLastChat(chatroom.chatList).sender}
+              secondary={findLastChat(chatroom.chatList).message}
+            />
+          </>
+        ))}
     </StyledChats>
   );
 }
