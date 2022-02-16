3c77e8d03113b925194ff85a68b1935d3fdb2353
Jinho <breezjin@gmail.com>
2022-02-16 21:42:32 +0900

    [87] geo 정보 javascript로는 잘 가져오는지 체크

--- src/common/api/getMyLongLat.js
+++ src/common/api/getMyLongLat.js
@@ -2,10 +2,7 @@ function getMyLongLat() {
   return new Promise((resolve, reject) => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
-        resolve({
-          longitude: position.coords.longitude,
-          latitude: position.coords.latitude,
-        });
+        resolve([position.coords.latitude, position.coords.longitude]);
       }, reject);
     }
     throw new Error("navigator.geolocation is not supported!");
