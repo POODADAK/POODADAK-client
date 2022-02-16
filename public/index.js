16fc543e7af55079c1bdae7b8194a04eec5d2521
Jinho <breezjin@gmail.com>
2022-02-16 21:20:23 +0900

    [87] geo 정보 javascript로는 잘 가져오는지 체크

--- public/index.js
+++ public/index.js
@@ -1,28 +1,13 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-console */
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const lng = position.coords.longitude;
-        const lat = position.coords.latitude;
-        console.log("lat:", lat, "lng:", lng);
-        alert("lat:", lat, "lng:", lng);
-      },
-      (error) => {
-        console.log(error);
-        alert(error);
-      },
-      {
-        enableHighAccuracy: false,
-        maximumAge: 0,
-        timeout: Infinity,
-      }
-    );
-  } else {
-    console.log("no GPS");
-    alert("no GPS");
-  }
+function getMyLongLat() {
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      resolve([position.coords.longitude, position.coords.latitude]);
+    });
+  });
 }
 
-getLocation();
+getMyLongLat().then((result) => {
+  alert(result);
+});
