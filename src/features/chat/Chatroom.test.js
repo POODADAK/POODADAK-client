0c47dc2645f75b77d2059e796f3f9a84614582a8
DanK <ddad9583@gmail.com>
2022-02-18 23:04:46 +0900

    [312] Chatroom 컴포넌트 테스트 추가.

--- src/features/chat/Chatroom.test.js
+++ src/features/chat/Chatroom.test.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { render, screen } from "../../common/util/testUtils";
+import {
+  render,
+  screen,
+  cleanup,
+  fireEvent,
+} from "../../common/util/testUtils";
 import { initialState as loginInitialState } from "../login/loginSlice";
 import { initialState as mainInitialState } from "../main/mainSlice";
 import { initialState as toiletInitialState } from "../toilet/toiletSlice";
@@ -11,7 +16,28 @@ import {
   participantStatusOptions,
 } from "./chatSlice";
 
+jest.mock("../../common/middlewares/socketMiddleware", () => {
+  const originalModule = jest.requireActual(
+    "../../common/middlewares/socketMiddleware"
+  );
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    socketEmitted: () => ({
+      type: "test",
+    }),
+  };
+});
+
 describe("Chatroom", () => {
+  let initialReduxState = {
+    login: JSON.parse(JSON.stringify(loginInitialState)),
+    chat: JSON.parse(JSON.stringify(chatInitialState)),
+    main: JSON.parse(JSON.stringify(mainInitialState)),
+    toilet: JSON.parse(JSON.stringify(toiletInitialState)),
+  };
+
   const wrappedChatRoomComponent = (
     <BrowserRouter>
       <Routes>
@@ -25,42 +51,89 @@ describe("Chatroom", () => {
   });
 
   beforeEach(() => {
+    initialReduxState.login.userId = "tester";
     window.history.pushState({}, "", "/chatroomList/mockChatroomId");
   });
 
+  afterEach(() => {
+    initialReduxState = {
+      login: JSON.parse(JSON.stringify(loginInitialState)),
+      chat: JSON.parse(JSON.stringify(chatInitialState)),
+      main: JSON.parse(JSON.stringify(mainInitialState)),
+      toilet: JSON.parse(JSON.stringify(toiletInitialState)),
+    };
+
+    cleanup();
+  });
+
   test("should render 'no connected chat' statement when there's no socket connection.", () => {
-    render(wrappedChatRoomComponent);
+    render(wrappedChatRoomComponent, {
+      preloadedState: initialReduxState,
+    });
 
     expect(
       screen.getByText("현재 연결된 채팅이 없습니다.")
     ).toBeInTheDocument();
-
-    // screen.debug();
   });
 
   test("should render 'stranger has disconnected' when stranger disconnect", () => {
-    const chatInitialStateCopy = JSON.parse(JSON.stringify(chatInitialState));
-
-    chatInitialStateCopy.participantStatus =
+    initialReduxState.chat.participantStatus =
       participantStatusOptions.participantLeft;
 
     render(wrappedChatRoomComponent, {
-      preloadedState: {
-        login: loginInitialState,
-        chat: chatInitialStateCopy,
-        main: mainInitialState,
-        toilet: toiletInitialState,
-      },
+      preloadedState: initialReduxState,
     });
 
     expect(
       screen.getByText("상대방이 채팅을 종료 했습니다.")
     ).toBeInTheDocument();
-
-    screen.debug();
   });
 
   test("should render loaded chat list when first rendered", () => {
+    const testMessage = "It should render!";
+
+    initialReduxState.chat.chatList = [
+      {
+        sender: "tester",
+        message: testMessage,
+        date: new Date().toISOString(),
+      },
+    ];
+
+    render(wrappedChatRoomComponent, {
+      preloadedState: initialReduxState,
+    });
+
+    expect(screen.getByText(testMessage)).toBeInTheDocument();
+  });
+
+  test("when click go back it should get out of screen", () => {
+    jest.fn();
+
+    render(wrappedChatRoomComponent, {
+      preloadedState: initialReduxState,
+    });
+
+    fireEvent.click(screen.getAllByRole("button")[0]);
+
+    expect(() => screen.getByText("보내기")).toThrow();
+  });
+
+  test("when chat is submitted it should appear on screen", () => {
+    const testText = "테스트 문구입니다!";
+
+    render(wrappedChatRoomComponent, {
+      preloadedState: initialReduxState,
+    });
+
+    fireEvent.change(screen.getByRole("textbox"), {
+      target: { value: testText },
+    });
+
+    fireEvent.click(screen.getAllByRole("button")[1]);
+
+    expect(screen.getByText(testText)).toBeInTheDocument();
+
     screen.debug();
   });
 });
