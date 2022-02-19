c2b5c96b242655f16b49c20dc4287bc818c7feca
DanK <ddad9583@gmail.com>
2022-02-19 09:38:58 +0900

    [302] eslint 추가

--- src/features/chat/ChatroomList.js
+++ src/features/chat/ChatroomList.js
@@ -51,6 +51,7 @@ function ChatroomList() {
     }
 
     getLiveChatroomList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentChatroomId]);
 
   function findLastChat(chatList) {
