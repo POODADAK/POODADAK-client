8fc5676e3896164ae1e85530f2471c26748b3102
Jinho Yoon <breezjin@gmail.com>
2022-11-08 16:54:16 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ cd ../
 mkdir output
 cp -R ./[your-repository-name]/* ./output
 cp -R ./[your-repository-name]/* ./output_temp
+cp -R ./output_temp ./[your-repository-name]/
 cp -R ./output ./[your-repository-name]/
